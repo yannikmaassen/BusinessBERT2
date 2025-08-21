d5af6386ac3a56742203675f76ae4f7bca7eb3a9
Yannik Maaßen <yannik.maassen@t-online.de>
2025-08-21 15:12:29 +0200

    change abbreviations in dataset creation

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -4,11 +4,11 @@ from transformers import PreTrainedTokenizerBase
 
 
 class PretrainDataset(Dataset):
-    def __init__(self, examples, tokenizer: PreTrainedTokenizerBase, max_len: int,
+    def __init__(self, examples, tokenizer: PreTrainedTokenizerBase, max_length: int,
                  idx2: Dict[str, int], idx3: Dict[str, int], idx4: Dict[str, int]):
         self.examples = examples
-        self.tok = tokenizer
-        self.max_len = max_len
+        self.tokenizer = tokenizer
+        self.max_length = max_length
         self.idx2 = idx2
         self.idx3 = idx3
         self.idx4 = idx4
@@ -17,23 +17,24 @@ class PretrainDataset(Dataset):
         return len(self.examples)
 
     def __getitem__(self, idx) -> Dict[str, Any]:
-        ex = self.examples[idx]
-        enc = self.tok(
-            ex.text_a if ex.text_a else "",
-            ex.text_b if ex.text_b else None,
+        example = self.examples[idx]
+        encoding = self.tokenizer(
+            example.text_a if example.text_a else "",
+            example.text_b if example.text_b else None,
             truncation=True,
-            max_length=self.max_len,
+            max_length=self.max_length,
             padding=False,
             return_tensors=None,
         )
-        sic2 = self.idx2.get(ex.sic2, -100)
-        sic3 = self.idx3.get(ex.sic3, -100)
-        sic4 = self.idx4.get(ex.sic4, -100)
+        sic2 = self.idx2.get(example.sic2, -100)
+        sic3 = self.idx3.get(example.sic3, -100)
+        sic4 = self.idx4.get(example.sic4, -100)
+
         return {
-            "input_ids": enc["input_ids"],
-            "token_type_ids": enc.get("token_type_ids", [0] * len(enc["input_ids"])),
-            "attention_mask": enc["attention_mask"],
-            "sop_label": ex.sop_label,
+            "input_ids": encoding["input_ids"],
+            "token_type_ids": encoding.get("token_type_ids", [0] * len(encoding["input_ids"])),
+            "attention_mask": encoding["attention_mask"],
+            "sop_label": example.sop_label,
             "sic2": sic2,
             "sic3": sic3,
             "sic4": sic4,
