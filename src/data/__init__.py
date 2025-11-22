4e19160bd08a2eb83c98d93a7c93eff583b19422
Yannik Maaßen <yannik.maassen@t-online.de>
2025-11-22 16:03:10 +0100

    use full length chunkings of examples for NSP objective

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,6 +1,7 @@
 from .dataset_chunking import PretrainDataset
 from .dataset_nsp import PretrainDatasetWithNSP
 from .dataset_nsp_segment import PretrainDatasetWithNSPSegments
+from .dataset_nsp_max import PretrainDatasetWithNSPSegmentsMax
 from .dataset_sampling import PretrainDatasetRandomSampling, PretrainDatasetOnTheFly
 from .dataset_sampling_new import PretrainDatasetOnTheFlyNew
 from .collator import Collator
@@ -8,6 +9,7 @@ from .collator import Collator
 __all__ = [
     "PretrainDatasetWithNSP",
     "PretrainDatasetWithNSPSegments",
+    "PretrainDatasetWithNSPSegmentsMax",
     "PretrainDataset",
     "PretrainDatasetRandomSampling",
     "PretrainDatasetOnTheFly",
